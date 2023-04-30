2579f119f7554cb8418aaef39811781aa00a6708
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-04-29 21:22:20 -0300

    Update Client.java

--- rental - versao grupo/Client.java
+++ rental - versao grupo/Client.java
@@ -16,100 +16,78 @@ public class Client {
     TapesAlugadas.add(Rent);
   }
 
-  public String extrato() {
+
+
+  public String extrato(){
     final String fimDeLinha = System.getProperty("line.separator");
     double valorTotal = 0.0;
     int pontosDeAlugadorFrequente = 0;
     Iterator alugueis = TapesAlugadas.iterator();
     String resultado = "Registro de Alugueis de " + getNome() + fimDeLinha;
-
     while(alugueis.hasNext()) {
       double valorCorrente = 0.0;
       Rent cada = (Rent)alugueis.next();
-      valorCorrente += calcula_preco(cada);
-
-      // trata de pontos de alugador frequente
-      pontosDeAlugadorFrequente++;
-      // adiciona bonus para Rent de um lan�amento por pelo menos 2 dias
-      if(dar_ponto_alugador(cada)){
-        pontosDeAlugadorFrequente++;
-      }
-
-      // mostra valores para este Rent
-      resultado += add_rent_resultado(cada,valorCorrente,fimDeLinha);
-      valorTotal += valorCorrente;
-    } // while
-
-    resultado += add_rodape_resultado(valorTotal,fimDeLinha,pontosDeAlugadorFrequente);
-    return resultado;
-  }
-
-  public double calcula_preco(Rent rent){
-    double valor = 0;
-    switch(rent.getCodigoPreco()) {
-      case Tape.NORMAL:
-        valor += 2;
-        if(rent.getDiasAlugada() > 2) {
-          valor += (rent.getDiasAlugada() - 2) * 1.5;
-        }
-        break;
-      case Tape.LANCAMENTO:
-        valor += rent.getDiasAlugada() * 3;
-        break;
-      case Tape.INFANTIL:
-        valor += 1.5;
-        if(rent.getDiasAlugada() > 3) {
-          valor += (rent.getDiasAlugada() - 3) * 1.5;
-        }
-        break;
+      valorCorrente = calculaExtrato(cada);
+      resultado += "\t" + cada.getTape().getTitulo() + "\t" + valorCorrente + fimDeLinha;
     }
-    return valor;
-  }
-
-  public boolean dar_ponto_alugador(Rent rent){
-    /*
-    if(rent.getTape().getCodigoDePreco() == Tape.LANCAMENTO &&
-            rent.getDiasAlugada() > 1){
-      return true;
-    }
-    return false; */
-
-    if((checa_lancamento(rent))&&(checa_dias_aluguel(rent,1))){
-      return true;
-    }
-    return false;
-  }
-
-  public boolean checa_lancamento(Rent rent){
-    if(rent.ehLancamento()){
-      return true;
-    }
-    return false;
-  }
-
-  public boolean checa_dias_aluguel(Rent rent, int dias){
-    /*
-    if(rent.getDiasAlugada() > dias){
-      return true;
-    }
-    return false;
-     */
-    return (rent.boolDiasAlugada(dias));
-  }
-
-  public String add_rent_resultado(Rent rent, double valorCorrente, String fimDeLinha){
-    String resultado = "";
-    resultado += "\t" + rent.getTape().getTitulo() + "\t" + valorCorrente + fimDeLinha;
-    return resultado;
-  }
-
-  public String add_rodape_resultado(double valorTotal, String fimDeLinha, int pontosDeAlugadorFrequente){
-    String resultado = "";
+    valorTotal = calculaExtratoTotal();
+    pontosDeAlugadorFrequente = givePontosAlugadorTotal();
     resultado += "Valor total devido: " + valorTotal + fimDeLinha;
     resultado += "Voce acumulou " + pontosDeAlugadorFrequente +
             " pontos de alugador frequente";
     return resultado;
   }
 
-}
 
+  public String extratoHTML(){
+    final String fimDeLinha = System.getProperty("line.separator");
+    double valorTotal = 0.0;
+    int pontosDeAlugadorFrequente = 0;
+    Iterator alugueis = TapesAlugadas.iterator();
+    String resultado = "<h1>"+"Registro de Alugueis de " + getNome() + "</h1>"+fimDeLinha;
+    while(alugueis.hasNext()) {
+      double valorCorrente = 0.0;
+      Rent cada = (Rent)alugueis.next();
+      valorCorrente = calculaExtrato(cada);
+      resultado += "<p>" + "\t" +cada.getTape().getTitulo() + "\t" + valorCorrente +"</p>" + fimDeLinha;
+    }
+    valorTotal = calculaExtratoTotal();
+    pontosDeAlugadorFrequente = givePontosAlugadorTotal();
+    resultado += "<p>"+"Valor total devido: " + valorTotal+"</p>" + fimDeLinha;
+    resultado +="<p>"+ "Voce acumulou " + pontosDeAlugadorFrequente +
+            " pontos de alugador frequente"+"</p>";
+    return resultado;
+  }
+
+
+  public double calculaExtrato(Rent cada){
+
+    return (cada.getValorAluguel());
+  }
+
+  public double calculaExtratoTotal(){
+    double valorCorrente = 0;
+    Iterator alugueis = TapesAlugadas.iterator();
+    while(alugueis.hasNext()) {
+      Rent cada = (Rent)alugueis.next();
+      valorCorrente+= calculaExtrato(cada);
+    }
+    return valorCorrente;
+  }
+
+  public int givePontosAlugador(Rent cada){
+    return cada.givePontosAlugador();
+  }
+
+  public int givePontosAlugadorTotal(){
+    int pontosDeAlugadorFrequente=  0;
+    Iterator alugueis = TapesAlugadas.iterator();
+    while(alugueis.hasNext()) {
+      Rent cada = (Rent)alugueis.next();
+      pontosDeAlugadorFrequente+= givePontosAlugador(cada);
+    }
+
+    return pontosDeAlugadorFrequente;
+  }
+
+}
