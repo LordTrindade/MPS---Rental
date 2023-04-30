7b56e95253d73bdf380daff41188b4b3a7dd56d1
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-04-29 21:23:17 -0300

    Update Rent.java

--- rental - versao grupo/Rent.java
+++ rental - versao grupo/Rent.java
@@ -15,21 +15,12 @@ public class Rent {
     return diasAlugada;
   }
 
-  public boolean boolDiasAlugada(int dias){
-    if (getDiasAlugada()>dias){
-      return true;
-    }
-    return false;
-  }
-  //funcao q retorna true se a quantidade de dias alugadas for maior doq um inteiro arbitrario
-
-  public boolean ehLancamento(){
-    return Tape.ehLancamento();
+  public double getValorAluguel(){
+    return (Tape.getValorAluguel(getDiasAlugada()));
   }
 
-  public int getCodigoPreco(){
-    return Tape.getCodigoDePreco();
+  public int givePontosAlugador(){
+    return Tape.givePontosAlugador(getDiasAlugada());
   }
 
 }
-
